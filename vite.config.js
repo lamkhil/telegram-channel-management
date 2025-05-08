694f4fa86e24788c801a76479b5987ec6d92aef4
lamkhil <lamkhil3000@gmail.com>
2025-05-08 13:09:52 +0700

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css', 
                 'resources/js/app.js',
-                
+                'resources/css/filament/admin/theme.css',
+                'resources/css/filament/public/theme.css'
             ],
             refresh: true,
         }),
