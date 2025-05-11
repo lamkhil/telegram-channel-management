d4cd158e6b709b56a230f79d886fe5bfe97d916c
lamkhil <lamkhil3000@gmail.com>
2025-05-12 02:01:49 +0700

    chane tutorial to youtube

--- resources/views/filament/pages/public/tutorial.blade.php
+++ resources/views/filament/pages/public/tutorial.blade.php
@@ -1,3 +1,8 @@
 <x-filament-panels::page>
-{{ $this->form }}
+    <div class="responsive-iframe-container" style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;width:100%;">
+        <iframe src="https://www.youtube.com/embed/bWaRomPA3r8?si=cjL1W6E8bUnyvijz" title="YouTube video player"
+                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen
+                style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+    </div>
 </x-filament-panels::page>
